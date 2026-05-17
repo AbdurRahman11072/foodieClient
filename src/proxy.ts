b9e8c9a29417fa2b033d8414ed197eb94d9aa6d9
AbdurRahman11072 <rjrahman014@gmail.com>
2026-05-17 08:31:49 +0600

    Refactor proxy function for consistent formatting and improved readability

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,13 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import { userRole } from './constants';
-import { userService } from './services/user.service';
+import { NextResponse, type NextRequest } from "next/server";
+import { userRole } from "./constants";
+import { userService } from "./services/user.service";
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
   const userSession = await userService.getUserSession();
+
+  console.log("proxy", userSession);
+
   const currentPath = request.nextUrl.pathname;
   // console.log(currentPath);
 
@@ -15,41 +18,42 @@ export async function proxy(request: NextRequest) {
 
   // console.log(isUserAuthorized);
 
-  if (currentPath.startsWith('/dashboard') && !isUserAuthorized) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (currentPath.startsWith("/dashboard") && !isUserAuthorized) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  if (userSession && currentPath.startsWith('/login')) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (userSession && currentPath.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (userSession && currentPath.startsWith('/sign-up')) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (userSession && currentPath.startsWith("/sign-up")) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  if (!userSession && currentPath.startsWith('/orders')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!userSession && currentPath.startsWith("/orders")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
-  if (!userSession && currentPath.startsWith('/checkout')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!userSession && currentPath.startsWith("/checkout")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   if (
-    currentPath.startsWith('/restaurants/create-restaurant') &&
-    userSession && (userSession.user as any)?.restaurantId
+    currentPath.startsWith("/restaurants/create-restaurant") &&
+    userSession &&
+    (userSession.user as any)?.restaurantId
   ) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 }
 
 export const config = {
   matcher: [
-    '/',
-    '/orders',
-    '/checkout',
-    '/restaurants',
-    '/restaurants/:path*',
-    '/login',
-    '/sign-up',
-    '/dashboard',
-    '/dashboard/:path*',
+    "/",
+    "/orders",
+    "/checkout",
+    "/restaurants",
+    "/restaurants/:path*",
+    "/login",
+    "/sign-up",
+    "/dashboard",
+    "/dashboard/:path*",
   ],
 };
